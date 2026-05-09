e5c390f510f2ef6d10edc6ebaec7b3443029e497
Jay Sexton <57783679+sayjexton@users.noreply.github.com>
2026-05-09 14:35:57 -0400

    more changes

--- drive.py
+++ drive.py
@@ -18,6 +18,7 @@ front_us = DistanceSensor(trigger=21, echo=19)
 left_us = DistanceSensor(trigger=0, echo=5)
 right_us = DistanceSensor(trigger=14, echo=15)
 back_us = DistanceSensor(trigger=20, echo=16)
+limit = 15
 
 def servo_up():
 	left_servo.value = 0.9
@@ -26,6 +27,12 @@ def servo_up():
 def servo_detach():
 	left_servo.detach()
 	right_servo.detach()
+
+def oscar_stop():
+	front_left_motor.stop()
+	front_right_motor.stop()
+	back_left_motor.stop()
+	back_right_motor.stop()
 	
 def oscar_forward(speed):
 	front_left_motor.forward(speed)
@@ -39,57 +46,73 @@ def oscar_backward(speed):
 	back_left_motor.backward(speed)
 	back_right_motor.backward(speed)
 
-def oscar_point_left(speed):
-	front_left_motor.backward(speed)
-	front_right_motor.forward(speed)
-	back_left_motor.backward(speed)
-	back_right_motor.forward(speed)
-
-def oscar_point_right(speed):
-	front_left_motor.forward(speed)
-	front_right_motor.backward(speed)
-	back_left_motor.forward(speed)
-	back_right_motor.backward(speed)
-
-def oscar_point_deg(deg):
-	t = (0.77*deg)/90
-	
-	if (deg > 0 and deg > 90): # fix the paramaters on these
-		oscar_point_left(1)
+def oscar_point_left(speed, deg):
+	if (deg != "inf"):
+		t = (0.77*deg)/90
+		front_left_motor.backward(speed)
+		front_right_motor.forward(speed)
+		back_left_motor.backward(speed)
+		back_right_motor.forward(speed)
 		sleep(t)
 		oscar_stop()
-		sleep(100)
-	elif (deg < 90 and deg <):
-		oscar_point_right(1)
-		sleep(t)
-		oscar_stop()
-		sleep(100)
-	elif (deg == 90):
-		oscar_point_left(1)
-		sleep(t)
-		oscar_stop()
-		sleep(100)
+	elif(deg == "inf"):
+		front_left_motor.backward(speed)
+		front_right_motor.forward(speed)
+		back_left_motor.backward(speed)
+		back_right_motor.forward(speed)
 	else:
-		print("Error, turn radius not in (0,2pi).")
+		print("Invalid degree measure input pointing right.")
 
-def oscar_strafe_left(speed):
-	front_left_motor.backward(speed)
-	front_right_motor.forward(speed)
-	back_left_motor.forward(speed)
-	back_right_motor.backward(speed)
+def oscar_point_right(speed, deg):
+	if (deg != "inf"):
+		t = (0.77*deg)/90
+		front_left_motor.forward(speed)
+		front_right_motor.backward(speed)
+		back_left_motor.forward(speed)
+		back_right_motor.backward(speed)
+		sleep(t)
+		oscar_stop()
+	elif (deg == "inf"):
+		front_left_motor.forward(speed)
+		front_right_motor.backward(speed)
+		back_left_motor.forward(speed)
+		back_right_motor.backward(speed)
+	else:
+		print("Invalid degree measure input pointing right.")
 
-def oscar_strafe_right(speed):
-	front_left_motor.forward(speed)
-	front_right_motor.backward(speed)
-	back_left_motor.backward(speed)
-	back_right_motor.forward(speed)
+def oscar_spin():
+	t = 0.77*4
+	oscar_point_left(1)
+	sleep(t)
+	oscar_stop()
+	sleep(0.5)
+	oscar_point_right(1)
+	sleep(t)
+	oscar_stop()
 
-def oscar_stop():
-	front_left_motor.stop()
-	front_right_motor.stop()
-	back_left_motor.stop()
-	back_right_motor.stop()
-
+# Object avoidance
 while True:
 	servo_detach()
-	oscar_point_deg(30)
+	
+	# check if there are objects
+	distance_front = front_us.distance() * 100
+	distance_back = back_us.distance() * 100
+	distance_left = left_us.distance() * 100
+	distance_right = right_us.distance() * 100
+
+	if (distance_front >= limit and distance_back >= limit and distance_left >= limit and distance_right >= limit):
+		oscar_forward()
+	else:
+		oscar_stop()
+		sleep(1)
+		if (distance_front < limit):
+			oscar_point_left(1, 90)
+			sleep(0.5)
+			oscar_point_right(1, 180)
+			sleep(0.5)
+		elif (distance_back < limit):
+			print()
+		elif (distance_left < limit):
+			print()
+		elif (distance_right < limit):
+			print()
