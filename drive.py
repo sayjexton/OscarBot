e05b979887f843bcfe466e5361fc7adbc9a66467
Jay Sexton <57783679+sayjexton@users.noreply.github.com>
2026-05-08 21:45:08 -0400

    more motor updates

--- drive.py
+++ drive.py
@@ -4,10 +4,10 @@ from gpiozero import DistanceSensor
 from time import sleep
 
 # Motors
-front_left_motor = Motor(forward=25, backward=24)
-back_left_motor = Motor(forward=7, backward=8)
-back_right_motor = Motor(forward=22, backward=4)
-front_right_motor = Motor(forward=9, backward=10)
+front_left_motor = Motor(forward=25, backward=24, enable=18)
+back_left_motor = Motor(forward=7, backward=8, enable=23)
+back_right_motor = Motor(forward=22, backward=4, enable=11)
+front_right_motor = Motor(forward=9, backward=10, enable=6)
 
 
 # Servos
@@ -15,10 +15,10 @@ left_servo = Servo(13)
 right_servo = Servo(1)
 
 # Ultrasonic sensors
-#front_us = DistanceSensor(trigger=21, echo=19)
-#left_us = DistanceSensor(trigger=0, echo=5)
-#right_us = DistanceSensor(trigger=14, echo=15)
-#back_us = DistanceSensor(trigger=20, echo=16)
+front_us = DistanceSensor(trigger=21, echo=19)
+left_us = DistanceSensor(trigger=0, echo=5)
+right_us = DistanceSensor(trigger=14, echo=15)
+back_us = DistanceSensor(trigger=20, echo=16)
 
 def servo_up():
 	left_servo.value = 0.9
@@ -30,10 +30,10 @@ def servo_detach():
 
 	
 def oscar_forward():
-	front_left_motor.forward()
-	front_right_motor.forward()
-	back_left_motor.forward()
-	back_right_motor.forward()
+	front_left_motor.forward(0.5)
+	front_right_motor.forward(0.5)
+	back_left_motor.forward(0.5)
+	back_right_motor.forward(0.5)
 	
 def oscar_backward():
 	front_left_motor.backward()
@@ -44,4 +44,4 @@ def oscar_backward():
 
 while True:
 	servo_detach()
-	oscar_backward()
+	oscar_forward()
