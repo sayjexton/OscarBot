4880bcef54e591a1741828556bb91bc3e498da24
Jay Sexton <57783679+sayjexton@users.noreply.github.com>
2026-05-09 21:29:57 -0400

    implement apriltag detection

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# sigh
+import sys
+sys.path.append('/home/oscar/oscar_dev/env/lib/python3.13/site-packages')
+
 # drive
 from gpiozero import Motor
 from gpiozero import Servo
@@ -6,10 +10,12 @@ from gpiozero import DistanceSensor
 # vision
 import cv2 as cv
 from pupil_apriltags import Detector
-import numpy
 
 # general
+from time import time
 from time import sleep
+import numpy
+import random
 
 ########### ACTUATORS & SENSORS
 # motors
@@ -27,6 +33,7 @@ front_us = DistanceSensor(trigger=21, echo=19)
 left_us = DistanceSensor(trigger=0, echo=5)
 right_us = DistanceSensor(trigger=14, echo=15)
 back_us = DistanceSensor(trigger=20, echo=16)
+limit = 25
 
 def servo_up():
 	left_servo.value = 0.9
@@ -35,6 +42,12 @@ def servo_up():
 def servo_detach():
 	left_servo.detach()
 	right_servo.detach()
+     
+def oscar_stop():
+	front_left_motor.stop()
+	front_right_motor.stop()
+	back_left_motor.stop()
+	back_right_motor.stop()
 
 def oscar_forward(speed):
 	front_left_motor.forward(speed)
@@ -48,23 +61,49 @@ def oscar_backward(speed):
 	back_left_motor.backward(speed)
 	back_right_motor.backward(speed)
 
-def oscar_point_left(speed):
-	front_left_motor.backward(speed)
-	front_right_motor.forward(speed)
-	back_left_motor.backward(speed)
-	back_right_motor.forward(speed)
+def oscar_point_left(speed, deg):
+	if (deg != "inf"):
+		t = (0.77*deg)/90
+		front_left_motor.backward(speed)
+		front_right_motor.forward(speed)
+		back_left_motor.backward(speed)
+		back_right_motor.forward(speed)
+		sleep(t)
+		oscar_stop()
+	elif(deg == "inf"):
+		front_left_motor.backward(speed)
+		front_right_motor.forward(speed)
+		back_left_motor.backward(speed)
+		back_right_motor.forward(speed)
+	else:
+		print("Invalid degree measure input pointing right.")
 
-def oscar_point_right(speed):
-	front_left_motor.forward(speed)
-	front_right_motor.backward(speed)
-	back_left_motor.forward(speed)
-	back_right_motor.backward(speed)
-
-def oscar_stop():
-	front_left_motor.stop()
-	front_right_motor.stop()
-	back_left_motor.stop()
-	back_right_motor.stop()
+def oscar_point_right(speed, deg):
+	if (deg != "inf"):
+		t = (0.77*deg)/90
+		front_left_motor.forward(speed)
+		front_right_motor.backward(speed)
+		back_left_motor.forward(speed)
+		back_right_motor.backward(speed)
+		sleep(t)
+		oscar_stop()
+	elif (deg == "inf"):
+		front_left_motor.forward(speed)
+		front_right_motor.backward(speed)
+		back_left_motor.forward(speed)
+		back_right_motor.backward(speed)
+	else:
+		print("Invalid degree measure input pointing right.")
+	
+def oscar_spin():
+	t = 0.77*4
+	oscar_point_left(1)
+	sleep(t)
+	oscar_stop()
+	sleep(0.5)
+	oscar_point_right(1)
+	sleep(t)
+	oscar_stop()
 
 ########### APRILTAG
 fx = 500
@@ -121,6 +160,31 @@ def at_get_theta(delta_x, h):
     theta_deg = numpy.rad2deg(theta_rad)
     return theta_deg
 
+def at_check(frame):	
+	gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+	results = at_detector.detect(
+    gray, 
+    estimate_tag_pose=True,
+    camera_params=at_detector_params,
+    tag_size=tag_size)
+	
+	if results:
+		return "found"
+	else:
+		return "not found"
+
+def at_poll(frame):
+	poll = []
+	start = time()
+	while ((time() - start) < 1):
+		poll.append(at_check(frame))
+		sleep(0.1)
+		
+	for result in poll:
+		if (result == "found"):
+			return "found"
+        
+
 ########### BLOB
 bDetector_params = cv.SimpleBlobDetector.Params()
 
@@ -175,7 +239,50 @@ if not camera.isOpened():
 ########## MAIN LOOP
 looping = True
 while looping:
-    print()
+	ret,frame = camera.read()
+	if not ret:
+		print("Camera returning no input.")
+		looping = False
+	
+	# check if there are objects
+	distance_front = front_us.distance * 100
+	distance_back = back_us.distance * 100
+	distance_left = left_us.distance * 100
+	distance_right = right_us.distance * 100
+	
+	# go forward if there are no obstacles
+	if (distance_front >= limit):
+		servo_detach()
+		oscar_forward(1)
+
+	# if there is an obstacle
+	else:
+		# stop and assess what would be an ideal path
+		oscar_stop()
+		sleep(1)
+		
+		if (distance_left > distance_right):
+			oscar_point_left(1, 90)
+			at_poll(frame)
+			sleep(1)
+		elif (distance_right < distance_left):
+			oscar_point_right(1, 90)
+			at_poll(frame)
+			sleep(1)
+		elif (distance_right == distance_left):
+			guess = random.randint(0,1)
+			if (guess == 0):
+				oscar_point_left(1, 90)
+				sleep(1)
+			else:
+				oscar_point_right(1, 90)
+				sleep(1)
+		else:
+			print("Oscar is lost.")
+
+	key = cv.waitKey(100)
+	if key == 13:
+	    looping = False
 
 camera.release()
 cv.destroyAllWindows()
