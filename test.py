cfe772994171d187a27b1bd4fbd78912707a67f9
Jay Sexton <57783679+sayjexton@users.noreply.github.com>
2026-05-09 22:55:26 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,101 @@
+from gpiozero import Motor
+from gpiozero import Servo
+from gpiozero import DistanceSensor
+from time import sleep
+import random
+
+import sys
+sys.path.append('/home/oscar/oscar_dev/env/lib/python3.13/site-packages')
+import cv2 as cv
+from pupil_apriltags import Detector
+import numpy
+
+# Motors
+front_left_motor = Motor(forward=25, backward=24, enable=18)
+back_left_motor = Motor(forward=7, backward=8, enable=23)
+back_right_motor = Motor(forward=22, backward=4, enable=11)
+front_right_motor = Motor(forward=9, backward=10, enable=6)
+
+# Servos
+left_servo = Servo(13)
+right_servo = Servo(1)
+
+# Ultrasonic sensors
+front_us = DistanceSensor(trigger=21, echo=19)
+left_us = DistanceSensor(trigger=0, echo=5)
+right_us = DistanceSensor(trigger=14, echo=15)
+back_us = DistanceSensor(trigger=20, echo=16)
+limit = 25
+
+def servo_up():
+	left_servo.value = 0.9
+	right_servo.value = 1
+
+def servo_detach():
+	left_servo.detach()
+	right_servo.detach()
+
+def oscar_stop():
+	front_left_motor.stop()
+	front_right_motor.stop()
+	back_left_motor.stop()
+	back_right_motor.stop()
+	
+def oscar_forward(speed):
+	front_left_motor.forward(speed)
+	front_right_motor.forward(speed)
+	back_left_motor.forward(speed)
+	back_right_motor.forward(speed)
+	
+def oscar_backward(speed):
+	front_left_motor.backward(speed)
+	front_right_motor.backward(speed)
+	back_left_motor.backward(speed)
+	back_right_motor.backward(speed)
+
+def oscar_point_left(speed, deg):
+	if (deg != "inf"):
+		t = (0.77*deg)/90
+		front_left_motor.backward(speed)
+		front_right_motor.forward(speed)
+		back_left_motor.backward(speed)
+		back_right_motor.forward(speed)
+		sleep(t)
+		oscar_stop()
+	elif(deg == "inf"):
+		front_left_motor.backward(speed)
+		front_right_motor.forward(speed)
+		back_left_motor.backward(speed)
+		back_right_motor.forward(speed)
+	else:
+		print("Invalid degree measure input pointing right.")
+
+def oscar_point_right(speed, deg):
+	if (deg != "inf"):
+		t = (0.77*deg)/90
+		front_left_motor.forward(speed)
+		front_right_motor.backward(speed)
+		back_left_motor.forward(speed)
+		back_right_motor.backward(speed)
+		sleep(t)
+		oscar_stop()
+	elif (deg == "inf"):
+		front_left_motor.forward(speed)
+		front_right_motor.backward(speed)
+		back_left_motor.forward(speed)
+		back_right_motor.backward(speed)
+	else:
+		print("Invalid degree measure input pointing right.")
+
+def oscar_spin():
+	t = 0.77*4
+	oscar_point_left(1)
+	sleep(t)
+	oscar_stop()
+	sleep(0.5)
+	oscar_point_right(1)
+	sleep(t)
+	oscar_stop()
+
+# Object avoidance
+oscar_point_left(1,90)
