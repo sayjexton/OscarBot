a04a6022e37e306472ffb62c6a699b29ec3b7645
Jay Sexton <57783679+sayjexton@users.noreply.github.com>
2026-05-09 23:12:24 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,26 +1,34 @@
+# sigh
+import sys
+sys.path.append('/home/oscar/oscar_dev/env/lib/python3.13/site-packages')
+
+# drive
 from gpiozero import Motor
 from gpiozero import Servo
 from gpiozero import DistanceSensor
-from time import sleep
-import random
 
-import sys
-sys.path.append('/home/oscar/oscar_dev/env/lib/python3.13/site-packages')
+# vision
 import cv2 as cv
 from pupil_apriltags import Detector
-import numpy
 
-# Motors
+# general
+from time import time
+from time import sleep
+import numpy
+import random
+
+########### ACTUATORS & SENSORS
+# motors
 front_left_motor = Motor(forward=25, backward=24, enable=18)
 back_left_motor = Motor(forward=7, backward=8, enable=23)
 back_right_motor = Motor(forward=22, backward=4, enable=11)
 front_right_motor = Motor(forward=9, backward=10, enable=6)
 
-# Servos
+# servos
 left_servo = Servo(13)
 right_servo = Servo(1)
 
-# Ultrasonic sensors
+# us sensors
 front_us = DistanceSensor(trigger=21, echo=19)
 left_us = DistanceSensor(trigger=0, echo=5)
 right_us = DistanceSensor(trigger=14, echo=15)
@@ -34,13 +42,13 @@ def servo_up():
 def servo_detach():
 	left_servo.detach()
 	right_servo.detach()
-
+     
 def oscar_stop():
 	front_left_motor.stop()
 	front_right_motor.stop()
 	back_left_motor.stop()
 	back_right_motor.stop()
-	
+
 def oscar_forward(speed):
 	front_left_motor.forward(speed)
 	front_right_motor.forward(speed)
@@ -86,7 +94,7 @@ def oscar_point_right(speed, deg):
 		back_right_motor.backward(speed)
 	else:
 		print("Invalid degree measure input pointing right.")
-
+	
 def oscar_spin():
 	t = 0.77*4
 	oscar_point_left(1)
@@ -97,5 +105,150 @@ def oscar_spin():
 	sleep(t)
 	oscar_stop()
 
-# Object avoidance
-oscar_point_left(1,90)
+########### APRILTAG
+fx = 500
+fy = 500
+cx = 500
+cy = 500
+
+at_detector = Detector(families='tag36h11')
+at_detector_params = (fx, fy, cx, cy)
+tag_size = 0.762
+
+def at_get_delta_x(frame):
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    results = at_detector.detect(
+        gray, 
+        estimate_tag_pose=True,
+        camera_params=at_detector_params,
+        tag_size=tag_size)
+
+    for r in results:
+        x_dist = r.pose_t[0]
+        return x_dist
+    
+def at_get_h(frame):
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    results = at_detector.detect(
+        gray, 
+        estimate_tag_pose=True,
+        camera_params=at_detector_params,
+        tag_size=tag_size)
+
+    for r in results:
+        h = r.pose_t[1]
+        return h
+
+def at_get_corners(frame):
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    results = at_detector.detect(
+        gray, 
+        estimate_tag_pose=True,
+        camera_params=at_detector_params,
+        tag_size=tag_size)
+
+    if results:
+        return results[0].corners[0],results[0].corners[2]
+    else:
+        return (1920,0),(0,1080)
+
+def at_get_theta(delta_x, h):
+    theta_rad = numpy.arcsin(delta_x/h)
+    theta_deg = numpy.rad2deg(theta_rad)
+    return theta_deg
+
+def at_check(frame):	
+	gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+	results = at_detector.detect(
+    gray, 
+    estimate_tag_pose=True,
+    camera_params=at_detector_params,
+    tag_size=tag_size)
+	
+	if results:
+		return "found"
+	else:
+		return "not found"
+
+def at_poll(frame):
+	poll = []
+	start = time()
+	while ((time() - start) < 1):
+		poll.append(at_check(frame))
+		sleep(0.1)
+		
+	for result in poll:
+		if (result == "found"):
+			return "found"
+        
+
+########### BLOB
+bDetector_params = cv.SimpleBlobDetector.Params()
+
+bDetector_params.filterByColor = True
+bDetector_params.blobColor = 0
+
+# thresholding (recoloring pixels to make them easier to differentiate between)
+bDetector_params.minThreshold = 10
+bDetector_params.maxThreshold = 200
+
+# circularity
+bDetector_params.filterByCircularity = True
+bDetector_params.minCircularity = 0.5
+
+# circularity
+bDetector_params.filterByArea = True
+bDetector_params.minArea = 100
+bDetector_params.maxArea = 10000000
+
+# uninterested
+bDetector_params.filterByConvexity = False
+bDetector_params.filterByInertia = False
+
+bDetector = cv.SimpleBlobDetector_create(bDetector_params)
+
+def find_blobs(frame, corner1, corner2):
+    x_start = int(corner2[0])
+    x_end = int(corner1[0])
+    y_start = int(corner1[1])
+    y_end = int(corner2[1])
+
+    if (x_start != 0):
+        cv.rectangle(frame,(x_start,y_start),(x_end,y_end),(255,255,255),-1)
+    
+    blob = bDetector.detect(frame)
+    
+    output = cv.drawKeypoints(frame, 
+                              blob, 
+                              numpy.array([]), 
+                              (0, 0, 0),
+                              cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    
+    cv.imshow("Blobs Detected", output)
+
+########## CAMERA SETUP
+camera = cv.VideoCapture(0)
+
+if not camera.isOpened():
+    print("Cannot open camera.")
+    exit()
+
+########## MAIN LOOP
+looping = True
+while looping:
+	ret,frame = camera.read()
+	if not ret:
+		print("Camera returning no input.")
+		looping = False
+	
+	print(at_check(frame))	
+		
+	key = cv.waitKey(100)
+	if key == 13:
+	    looping = False
+
+camera.release()
+cv.destroyAllWindows()
